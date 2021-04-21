d2cb41b0b5f85ab674c678d152c76b58e43ff758
yejin <yejin@ibleaders.co.kr>
2021-04-21 23:11:22 +0900

    head 태그 사용하기
    - 리액트에서 head 태그는 next/head를 import하여 사용

--- pages/signup.js
+++ pages/signup.js
@@ -1,6 +1,14 @@
+import React from 'react';
+import Head from 'next/head';
+
 const SignUp = () => {
   return (
+    <>
+      <Head>
+        <title>회원가입</title>
+      </Head>
     <div>회원가입 페이지</div>
+    </>
   )
 }
 
