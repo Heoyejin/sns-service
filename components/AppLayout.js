ef66220f12999957a06357ca7f074448194e8441
yejin <yejin@ibleaders.co.kr>
2021-04-21 23:12:56 +0900

    Login 폼 구현
    - ant Design의 Form, Input, Button 컴포넌트를 사용하여 구현
    - 1차적으로 더미데이터를 이용해서 로그인 되는 것 처럼 구현

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -3,8 +3,10 @@ import PropTypes from 'prop-types';
 import Link from 'next/link';
 import { Menu, Input, Row, Col  } from 'antd';
 
+import LoginForm from '../components/LoginForm';
 // index, profile, signup 페이지에서 공통적으로 사용할 레이아웃 생성
 const AppLayout = ({ children }) => {
+  const [isLogged, setIsLogged] = useState(false);
   return (
     <div>
       <Menu mode="horizontal">
@@ -21,6 +23,19 @@ const AppLayout = ({ children }) => {
         <Link href='/sigup'><a>회원가입</a></Link>
       </Menu.Item>
       </Menu> 
+      <Row gutter={8}>
+        {/* xs: 24=100% 기준으로  md:차지할 비율 */}
+        {/* https://ant.design/components/grid/ 참고*/}
+        <Col xs={24} md={6}>
+          { isLogged ? <UserProfile setIsLogged={setIsLogged} /> : <LoginForm setIsLogged={setIsLogged} /> }
+        </Col>
+        <Col xs={24} md={12}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}>
+          <a href="https://demo.ibleaders.com/ibsheet/v8/samples/customer-sample/html/main.html" target="_blank" rel="noreferrer noopener">IBSheet Demo</a>
+        </Col>
+      </Row>
     </div>
   )
 }
