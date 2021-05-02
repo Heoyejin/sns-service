74de9d96d469f8bba72f8af8c56e467fb97e5784
yejin <yejin@ibleaders.co.kr>
2021-05-02 14:38:35 +0900

    css 오류 수정 및 package.json 수정

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import PropTypes from 'prop-types';
+import React from 'react';
 // head 태그를 수정하려면 next의 head 컴포넌트를 사용해야함.
 import Head from 'next/head';
-// import 'antd/dist/antd.css';
+import PropTypes from 'prop-types';
+import 'antd/dist/antd.css';
 
 const App = ({ Component }) => {
   return (
@@ -15,8 +16,8 @@ const App = ({ Component }) => {
   )
 }
 
-App.PropTypes = {
-  Component: PropTypes.node.isRequired,
+App.propTypes = {
+  Component: PropTypes.elementType.isRequired,
 }
 
 export default App;
