2293f2de511a877b0f8d3227f58bc780def528a3
yejin <yejin@ibleaders.co.kr>
2021-10-21 22:21:53 +0900

    로그인하지 않은 사용자가 프로필 페이지로 이동하는 경우 처리

--- pages/profile.js
+++ pages/profile.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Head from 'next/head';
 import AppLayout from '../components/AppLayout';
 import NicknameEditForm from '../components/NickNameEditForm';
 import FollowList from '../components/FollowList';
 import { useSelector } from 'react-redux';
+import { useRouter } from 'next/router';
 
 const Profile = () => {
   const { me } = useSelector((state) => state.user);
+  const router = useRouter();
   
+  // 로그인 하지 않고 프로필 화면으로 가는 경우 오류
+  useEffect(() => {
+    if (!(me && me.id)) {
+      router.push('/');
+    }
+  }, [me && me.id]);
+
+  if (!me) return null;
+
   return (
     <>
       <Head>
