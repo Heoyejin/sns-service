c0586d01c7b4f41060034cdda931f71bd4c24ade
yejin <yejin@ibleaders.co.kr>
2022-01-23 19:01:55 +0900

    sequelize로 정의한 경우 주석 내용 처럼 method처럼 사용 가능함

--- back/models/post.js
+++ back/models/post.js
@@ -11,15 +11,15 @@ module.exports = (sequelize, DataTypes) => {
   });
   
   Post.associate = (db) => {
-    db.Post.belongsTo(db.User),
+    db.Post.belongsTo(db.User), // post.addUser, post.getUser, post.setUser
     // 다 대 다 관계는 belongsToMany
-    db.Post.belongsToMany(db.Hashtag, { through: 'PostHashtag' }),
-    db.Post.hasMany(db.Comment),
-    db.Post.hasMany(db.Image),
+    db.Post.belongsToMany(db.Hashtag, { through: 'PostHashtag' }),  // post.addHashtags
+    db.Post.hasMany(db.Comment),  // post.addComments, post.getComments
+    db.Post.hasMany(db.Image),  // post.addImages, post.getImages
     // 좋아요 설정 - through로 중간 테이블 이름을 바꿔줌
-    db.Post.belongsToMany(db.User, { through: 'Like', as: 'Likers' }),
+    db.Post.belongsToMany(db.User, { through: 'Like', as: 'Likers' }),  // post.addLikers, post.removeLikers
     // 리트윗해서 복사되는 원본 게시글 하나에 복사되는 게시글 여러개가 생김
-    db.Post.belongsTo(db.Post, { as : 'Retweet'})
+    db.Post.belongsTo(db.Post, { as : 'Retweet'}) //post.addRetweet
   };
   return Post;
 }
