21e45fde2ce0a58cfb2bace9817153ce559be4db
yejin <yejin@ibleaders.co.kr>
2022-01-23 18:51:38 +0900

    로그아웃 버그 수정

--- front/sagas/user.js
+++ front/sagas/user.js
@@ -58,17 +58,12 @@ function* logIn(action) {
 
 /****** LogOut ******/
 function logOutAPI() {
-  return axios.post('/api/logout');
+  return axios.post('/user/logout');
 }
 
 function* logOut() {
-  console.log('sagas-user-logout');
   try {
-    // 서버 요청 결과를 받아서 success/failure 로 Action을 나눠 주는 구간
-    // put - dispatch와 비슷한 역할을 하는 effects라고 생각 하면 됨
-    // call - 비동기 함수 호출, fork - 동기 함수 호출
-    // const result = yield call(logOutAPI);
-    yield delay(1000);
+    yield call(logOutAPI);
     yield put({
       type: LOG_OUT_SUCCESS,
     });
