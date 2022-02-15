480540f453f7c44a28eab8a14326ce25d51b228f
허예진 <yejin@ibleaders.co.kr>
2022-02-15 21:19:01 +0900

    model 문법 최신화
     - TODO: 나머지 테이블도 구현하기

--- back/models/comment.js
+++ back/models/comment.js
@@ -1,20 +1,23 @@
-module.exports = (sequelize, DataTypes) => {
-  const Comment = sequelize.define('Comment', {
-    // id가 기본적으로 들어있는데 mySql에서 자동으로 만들어줌
-    content: {
-      type: DataTypes.TEXT,
-      allowNull: false
-    },
-    // belongTo를 선언하면 아래 처럼 컬럼 정보가 생성됨
-    // UserId: 1
-    // PostId: 3
-  }, {
-    charset: 'utf8mb4',
-    collate: 'utf8mb4_general_ci'  // 한글 + 이모티콘 저장
-  });
-  Comment.associate = (db) => {
+const DataTypes = require('sequelize');
+const { Model } = DataTypes;
+module.exports = class Comment extends Model {
+  static init(sequelize) {
+    return super.init({
+      content: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      }
+    }, {
+      modelName: 'Comment',
+      tableName: 'comment',
+      charset: 'utf8mb4',
+      collate: 'utf8mb4_general_ci',
+      sequelize,
+    });
+  }
+
+  static associate(db) {
     db.Comment.belongsTo(db.User),
     db.Comment.belongsTo(db.Post)
-  };
-  return Comment;
-}
+  }
+};
