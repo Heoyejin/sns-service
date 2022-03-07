d27605d67af7885bb7d6e0fcaa4c5e79aaab1930
허예진 <yejin097@naver.com>
2022-03-07 22:16:25 +0900

    feat: 프로필 스타일 변경

--- front/components/UserProfile.js
+++ front/components/UserProfile.js
@@ -18,19 +18,20 @@ const UserProfile = () => {
     <>
     <Card
       actions={[
-        <div key="twit"><Link href={`user/${me.id}`}><a>으음<br />{me.Posts.length}</a></Link></div>,
+        <div key="twit"><Link href={`user/${me.id}`}><a>게시물<br />{me.Posts.length}</a></Link></div>,
         <div key="followings"><Link href={`user/${me.id}`}><a>팔로잉<br />{me.Followings.length}</a></Link></div>,
         <div key="follower"><Link href={`user/${me.id}`}><a>팔로워<br />{me.Followers.length}</a></Link></div>
       ]}>
+      <Button onClick={ onLogout } style={{ float: 'right' }} loading={ logOutLoading }>로그아웃</Button>
       <Card.Meta
         avatar={(
           <Link href={`/user/${me.id}`}>
             <a><Avatar>{me.nickname[0]}</Avatar></a>
           </Link>
         )}
-        title={ me.nickname }
+        title={ me.email.split('@')[0] }
+        description= { me.nickname }
       />
-      <Button onClick={onLogout} loading={ logOutLoading }>로그아웃</Button>
     </Card>
     </>
   )
